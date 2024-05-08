114f824e4a45094077422d0ab649203f24ae6014
nedaarsenijevic <arsenijevicneda1@gmail.com>
2024-05-08 19:35:56 +0200

    Frontend

---
+++ frontend/IT-33-2021-Frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ArtiklComponent } from './main/artikl/artikl.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArtiklComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
